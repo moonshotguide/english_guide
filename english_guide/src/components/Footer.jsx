3bec1afabb02c60ce8ef52ada7da530dcbabcbdc
moonshotguide <114834963+moonshotguide@users.noreply.github.com>
2023-03-20 00:54:18 +0000

    gradients colors btn

--- english_guide/src/components/Footer.jsx
+++ english_guide/src/components/Footer.jsx
@@ -1,10 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { LanguageContext } from "./context/LanguageContext";
 
 const Header = () => {
   const { language } = useContext(LanguageContext);
+  const [gradientColors, setGradientColors] = useState(["#00dfd8", "#007cf0"]); // Definir los colores iniciales del gradiente
+
+  useEffect(() => {
+    // Cambiar los colores del gradiente cada 5 segundos
+    const interval = setInterval(() => {
+      setGradientColors(getRandomColors());
+    }, 3800);
+    return () => clearInterval(interval);
+  }, []);
+
+  const getRandomColors = () => {
+    // Generar dos colores aleatorios para el gradiente
+    const color1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const color2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return [color1, color2];
+  };
 
   return (
     <footer className="py-3.5 px-5 md:flex md:items-center md:justify-between dark:bg-black">
@@ -16,13 +32,15 @@ const Header = () => {
         <IoArrowBack fontSize={19.5} className="mr-2" />
         {language === "english" ? "Back" : "Volver"}
       </button>
-      <div>
-        <span className="bg-gradient-to-br from-teal-400 to-blue-500"/>
-        <span/>
-        <span/>
+      <div
+        className="search-container"
+        style={{
+          background: `linear-gradient(45deg, ${gradientColors[0]}, ${gradientColors[1]})`,
+        }}
+      >
         <Link
           to="/"
-          className="bg-black text-white inline-flex px-3 py-3 font-bold text-center uppercase align-middle rounded-lg cursor-pointer leading-normal text-xs transition-all"
+          className="search-input dark:bg-black bg-white dark:hover:bg-transparent hover:bg-transparent dark:text-white dark:hover:text-white text-black hover:text-white inline-flex px-3 py-3 font-bold text-center uppercase align-middle rounded-lg cursor-pointer leading-normal text-xs transition-all"
         >
           <IoArrowBack fontSize={19.5} className="mr-2" />
           {language === "english" ? "Home" : "Inicio"}
