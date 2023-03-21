fcf72ec79934f9abf304fef9b821692f4205a423
moonshotguide <114834963+moonshotguide@users.noreply.github.com>
2023-03-21 01:02:03 +0000

    robbery card

--- english_guide/src/components/Footer.jsx
+++ english_guide/src/components/Footer.jsx
@@ -1,49 +1,75 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { IoArrowBack } from "react-icons/io5";
+import { IoArrowBack, IoHome } from "react-icons/io5";
 import { LanguageContext } from "./context/LanguageContext";
+import { gsap } from "gsap";
 
-const Header = () => {
+const Footer = () => {
   const { language } = useContext(LanguageContext);
-  const containerRef = useRef(null);
-  const gradients = [
-    "linear-gradient(45deg, #00dfd8, #007cf0)",
-    "linear-gradient(45deg, #ff4d4d, #f9cb28)",
-    "linear-gradient(45deg, #ff0080, #7928ca)"
-  ];
+  const gradients = useMemo(
+    () => [
+      { name: "Gradient 1", colors: ["#4b6cb7", "#182848"] },
+      { name: "Gradient 2", colors: ["#f857a6", "#ff5858"] },
+      { name: "Gradient 3", colors: ["#00c6ff", "#0072ff"] },
+    ],
+    []
+  );
+
   const [gradientIndex, setGradientIndex] = useState(0);
+
+  const backgroundRef = useRef(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setGradientIndex((prevIndex) => (prevIndex + 1) % gradients.length);
+      setGradientIndex(prevIndex => (prevIndex + 1) % gradients.length);
     }, 5000);
     return () => clearInterval(interval);
   }, [gradients]);
 
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: -1 });
+    tl.to(backgroundRef.current, { duration: 4, keyframes: [
+      {
+        background: `linear-gradient(to right, ${gradients[0].colors.join(", ")})`,
+      },
+      {
+        background: `linear-gradient(to right, ${gradients[1].colors.join(", ")})`,
+      },
+      {
+        background: `linear-gradient(to right, ${gradients[2].colors.join(", ")})`,
+      },
+      {
+        background: `linear-gradient(to right, ${gradients[0].colors.join(", ")})`,
+      }
+    ] });
+  }, [gradients]);
+
   return (
     <footer className="py-3.5 px-5 md:flex md:items-center md:justify-between dark:bg-black">
-      {/* Return Back Button */}
-      <button
-        className="dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white dark:hover:border-[#ffffff73]  bg-black text-white hover:bg-white hover:text-black hover:border-black border inline-flex px-3 py-3 font-bold text-center uppercase align-middle rounded-lg cursor-pointer leading-normal text-xs transition-all  "
-        onClick={() => window.history.back()}
-      >
-        <IoArrowBack fontSize={19.5} className="mr-2" />
-        {language === "english" ? "Back" : "Volver"}
-      </button>
+      {/* Home Button */}
       <div
         className="search-container"
-        style={{ background: gradients[gradientIndex] }}
+        ref={backgroundRef}
+        style={{ background: `linear-gradient(to right, ${gradients[gradientIndex].colors.join(", ")})` }}
       >
         <Link
           to="/"
           className="search-input dark:bg-black bg-white dark:hover:bg-transparent hover:bg-transparent dark:text-white dark:hover:text-white text-black hover:text-white inline-flex px-3 py-3 font-bold text-center uppercase align-middle rounded-lg cursor-pointer leading-normal text-xs transition-all"
         >
-          <IoArrowBack fontSize={19.5} className="mr-2" />
+          <IoHome fontSize={16} className="mr-2" />
           {language === "english" ? "Home" : "Inicio"}
         </Link>
       </div>
+      {/* Return Back Button */}
+      <button
+        className="dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white dark:hover:border-[#ffffff73]  bg-black text-white hover:bg-white hover:text-black hover:border-black border inline-flex px-3 py-2.5 font-bold text-center uppercase align-middle rounded-lg cursor-pointer leading-normal text-xs transition-all  "
+        onClick={() => window.history.back()}
+      >
+        <IoArrowBack fontSize={16} className="mr-2" />
+        {language === "english" ? "Back" : "Volver"}
+      </button>
     </footer>
   );
 };
 
-export default Header;
+export default Footer;
--- english_guide/src/screens/RobberyScreen.jsx
+++ english_guide/src/screens/RobberyScreen.jsx
@@ -1,13 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { LanguageContext } from "../components/context/LanguageContext";
+import { BsCheckCircleFill } from "react-icons/bs"
 
 const RobberyScreen = () => {
   const { language } = useContext(LanguageContext);
   return (
-    <div>
-      {language === "english" ? "Robbery" : "Robos"}
+    <div className="card flex flex-col justify-center items-center">
+      {/* Title */}
+      <span className="text-2xl font-extrabold ">
+        {language === "english" ? "Robbery" : "Robos"}
+      </span>
+      {/* Subtitle */}
+      <p>{language === "english" ? "Procedures" : "Procedimientos"}</p>
+      <h2>
+        {language === "english"
+          ? "What to say and what NOT to say?"
+          : "Que decir y que NO decir?"}
+      </h2>
+      {/* div form wrapper */}
+      <div className="flex flex-row items-center justify-between">
+        {/* What to say wrapper */}
+        <div className="flex flex-col align-center justify-between w-1/2">
+          <img src="#" alt="ToSay" />
+          <ul className="flex flex-col gap-1 justify-between align-center">
+            <li>{language === "english" ? "Intrussion signals" : "Señales de intrusion"}</li>
+            <li>{language === "english" ? "Tamper Signals" : "Señales tamper"}</li>
+            <li>{language === "english" ? "Jamming Signals" : "Señales inhibición"}</li>
+            <li>{language === "english" ? "Power cuts" : "Cortes de corriente"}</li>
+            <li>{language === "english" ? "Central Low/Dead battery" : "Batería baja/agotada de Central"}</li>
+            <li>{language === "english" ? "O/C Authenticated" : "O/C Controlados"}</li>
+          </ul>
+        </div>
+        {/* What to NOT say wrapper */}
+        <div className="flex flex-col align-center justify-between">
+          <img src="#" alt="ToNotSay" />
+          <ul className="flex flex-col gap-1 justify-between align-center w-1/2">
+            <li>{language === "english" ? "Missing Test" : "Fallos de comunicaciones"}</li>
+            <li>{language === "english" ? "Detectors' Low/Dead battery" : "Batería baja/agotada detectores"}</li>
+            <li>{language === "english" ? "Informative signs in mustard" : "Señales informativas en mostaza"}</li>
+            <li>{language === "english" ? "Pending maintenance" : "Mantenimientos pendientes"}</li>
+            <li>{language === "english" ? "Power cuts" : "Cortes de corriente"}</li>
+            <li>{language === "english" ? "Comlogs that differ from what is stated in the theft report" : "Comlogs que difieran con lo indicado en el informe de robo"}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default RobberyScreen;
